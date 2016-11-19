9ec8925444f818ea69ab96cb76fcffd6d3836cbb
Julliana Almeida <julliana.coutoalmeida@gmail.com>
2016-11-19 17:46:33 -0200

    Adding comandy to install shell and page default to virtualBox

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Created by freemanpivo - Chef installation script
+
+# Update system
+sudo apt-get update
+
+# Install git
+sudo apt-get install -y git
+
+# Install chef
+sudo apt-get install -y chef
